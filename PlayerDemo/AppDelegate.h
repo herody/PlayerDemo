04712cdc1726f2c8eece0f1ed0d9ed5374006cd0
herody <314622981@qq.com>
2017-07-12 16:33:01 +0800

    Initial Commit

---
+++ PlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayerDemo
+//
+//  Created by 侯亚迪 on 17/7/12.
+//  Copyright © 2017年 杭州魔品科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PlayerDemo
+//
+//  Created by 侯亚迪 on 17/7/12.
+//  Copyright © 2017年 杭州魔品科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
